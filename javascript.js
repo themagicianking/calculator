b446fd83c7c074f1974d344010a775fa68105557
Thane Chaves <tmoonder@gmail.com>
2021-10-31 13:56:43 -0400

    Added initial calculator functions

---
+++ javascript.js
@@ -0,0 +1,19 @@
+let add = function (n1, n2) {
+    let total = n1 += n2;
+    return total;
+} 
+
+let subtract = function (n1, n2) {
+    let total = n1 -= n2;
+    return total;
+}
+
+let multiply = function (n1, n2) {
+    let total = n1 *= n2;
+    return total;
+}
+
+let divide = function (n1, n2) {
+    let total = n1 /= n2;
+    return total;
+}
