ed6103c2914a39cf65491fe55a07bb787e3d85d1
Thane Chaves <tmoonder@gmail.com>
2021-10-31 14:03:24 -0400

    Added operator function.

--- javascript.js
+++ javascript.js
@@ -17,3 +17,18 @@ let divide = function (n1, n2) {
     let total = n1 /= n2;
     return total;
 }
+
+let operate = function (operator, n1, n2) {
+    if (operator === "add") {
+        add(n1, n2);
+    }
+    else if (operator === "subtract") {
+        subtract(n1, n2);
+    }
+    else if (operator === "multiply") {
+        multiply(n1, n2);
+    }
+    else {
+        divide(n1, n2);
+    }
+}
