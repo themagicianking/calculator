666c474abcd5b78d4548d9203be310602c5aeb7b
Thane Chaves <tmoonder@gmail.com>
2021-11-22 13:26:38 -0500

    Did an overhaul of most of the code to allow the calculator to evaluate only two expressions at a time.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,8 @@
 let total;
+let n1 = "clear";
+let n2 = "clear";
+let operator = "clear";
+let hasNewOperator = "yes";
 
 let add = function (n1, n2) {
     total = n1 += n2;
@@ -17,6 +21,9 @@ let multiply = function (n1, n2) {
 
 let divide = function (n1, n2) {
     total = n1 /= n2;
+    if (total === Infinity) {
+        total = "Nice try.";
+    }
     return total;
 }
 
@@ -35,127 +42,47 @@ let operate = function (operator, n1, n2) {
     }
 }
 
-let firstNumber;
-
 const display = document.querySelector("#display");
-
 const content = document.querySelector("#content");
 
-let storeNumberZero = function () {
+const digitKeys = document.querySelectorAll(".digit");
+
+function handleDigit(event) {
+    if (hasNewOperator === "yes") {
+        content.textContent = event.target.value;
+        hasNewOperator = "no";
+    }
+    else {
     let previous = content.textContent;
-    content.textContent = previous + "0";
+    content.textContent = previous + event.target.value;
+    }
 }
 
-document.getElementById("zero").addEventListener("click", storeNumberZero);
+digitKeys.forEach(
+    function(key) {
+        key.addEventListener("click", handleDigit);
+    }
+);
 
+const operatorKeys = document.querySelectorAll(".operator");
 
-let storeNumberOne = function () {
-    let previous = content.textContent;
-    content.textContent = previous + "1";
+function handleOperator(event) {
+    hasNewOperator = "yes";
+    if (n1 === "clear") {
+        n1 = parseInt(content.textContent);
+        operator = event.target.value;
+    }
+    else {
+        n2 = parseInt(content.textContent);
+        operate(operator, n1, n2);
+        operator = event.target.value;
+        content.textContent = total;
+        n1 = total;
+    }
 }
 
-document.getElementById("one").addEventListener("click", storeNumberOne);
-
-let storeNumberTwo = function () {
-    let previous = content.textContent;
-    content.textContent = previous + "2";
-}
-
-document.getElementById("two").addEventListener("click", storeNumberTwo);
-
-let storeNumberThree = function () {
-    let previous = content.textContent;
-    content.textContent = previous + "3";
-}
-
-document.getElementById("three").addEventListener("click", storeNumberThree);
-
-let storeNumberFour = function () {
-    let previous = content.textContent;
-    content.textContent = previous + "4";
-}
-
-document.getElementById("four").addEventListener("click", storeNumberFour);
-
-let storeNumberFive = function () {
-    let previous = content.textContent;
-    content.textContent = previous + "5";
-}
-
-document.getElementById("five").addEventListener("click", storeNumberFive);
-
-let storeNumberSix = function () {
-    let previous = content.textContent;
-    content.textContent = previous + "6";
-}
-
-document.getElementById("six").addEventListener("click", storeNumberSix);
-
-let storeNumberSeven = function () {
-    let previous = content.textContent;
-    content.textContent = previous + "7";
-}
-
-document.getElementById("seven").addEventListener("click", storeNumberSeven);
-
-let storeNumberEight = function () {
-    let previous = content.textContent;
-    content.textContent = previous + "8";
-}
-
-document.getElementById("eight").addEventListener("click", storeNumberEight);
-
-let storeNumberNine = function () {
-    let previous = content.textContent;
-    content.textContent = previous + "9";
-}
-
-document.getElementById("nine").addEventListener("click", storeNumberNine);
-
-let operator;
-
-let storeOperatorPlus = function () {
-    operator = "add";
-    n1 = parseInt(content.textContent);
-    content.textContent = "";
-}
-
-document.getElementById("plus").addEventListener("click", storeOperatorPlus);
-
-let storeOperatorMinus = function () {
-    operator = "subtract";
-    n1 = parseInt(content.textContent);
-    content.textContent = "";
-}
-
-document.getElementById("minus").addEventListener("click", storeOperatorMinus);
-
-let storeOperatorTimes = function () {
-    operator = "multiply";
-    n1 = parseInt(content.textContent);
-    content.textContent = "";
-}
-
-document.getElementById("times").addEventListener("click", storeOperatorTimes);
-
-let storeOperatorDivided = function () {
-    operator = "divide";
-    n1 = parseInt(content.textContent);
-    content.textContent = "";
-}
-
-document.getElementById("divided").addEventListener("click", storeOperatorDivided);
-
-let calculate = function () {
-    n2 = parseInt(content.textContent);
-    operate(operator, n1, n2);
-    content.textContent = total;
-}
-
-document.getElementById("equals").addEventListener("click", calculate);
-
-let clearAll = function () {
-    content.textContent = "";
-}
-
-document.getElementById("clear").addEventListener("click", clearAll);
+operatorKeys.forEach(
+    function(key) {
+        key.addEventListener("click", handleOperator);
+    }
+);
